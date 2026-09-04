315ac495cd924c947efd377060a1c539ed7848d3
light <lightbakpassim23@gmail.com>
2026-09-04 16:31:37 +0000

    ok

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCHlvCFiWB82SkRFE7AdYmxnTk4Iuqmtm4',
+  authDomain: 'edusphere-69403.firebaseapp.com',
+  projectId: 'edusphere-69403',
+  storageBucket: 'edusphere-69403.firebasestorage.app',
+  messagingSenderId: '912184776703',
+  appId: '1:912184776703:web:0613d35297dfe9af1239ca',
+}
+
+export const app = initializeApp(firebaseConfig)
